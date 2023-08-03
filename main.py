2bca6e7ce026d2a571bf6632d1756e194883ad59
ayu-05 <139468006+ayu-05@users.noreply.github.com>
2023-08-03 18:15:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+from tkinter import *
+from textblob import TextBlob                   ##language translation API
+from tkinter.scrolledtext import ScrolledText   ##for scrollable text box
+
+#-----translation function----------------------------------------------------------------------
+def toLang(lang):
+    try:
+        output.delete("1.0",END)            ##to delete previous entry in the output box
+        inputSTR = input_str.get("1.0",END)
+        obj = TextBlob(str(inputSTR))
+        outputSTR = obj.translate(to=lang)
+        output.insert(END,str(outputSTR))   ##insert output to the output box
+    except:
+        output.insert(END, "**Please enter a meaningful word/sentence**")
+
+        
+#-----user interface section---------------------------------------------------------------------
+root = Tk()
+root.title("Instant Translator")
+root.geometry("400x500+400+200")            ##width x height + xpos + ypos
+root.configure(bg = "orange")
+
+label1 = Label(root,text = "Translate from English to any language", fg="white",bg="black", font = ("Times New Roman","18"))
+label1.place(x=10,y=30)
+
+label2 = Label(root,text = "Type Anything:",fg="blue",font = ("Times New Roman","12"))
+label2.place(x=20,y=90)
+                                            ##scrollable text box with multiple lines of input
+input_str = ScrolledText(root,width=42,height=6,font=("Times New Roman","12"))
+input_str.place(x=20,y=120)
+
+label3 = Label(root,text = "Translate to :",fg="black",bg="yellow",font = ("Times New Roman","13"))
+label3.place(x=150,y=270)
+
+translate_button = Button(root,text = "TRANSLATE TO HINDI",command = toLang('hi'),fg="white",bg="green")
+translate_button.place(x=75,y=300
+                                            ##scrollable text box with multiple lines of output
+output = ScrolledText(root,width = 42,height=6,font=("Times New Roman","12"))
+output.place(x=20,y=350)
+
+credit_label = Label(root,text = "© Prakhar Doneria")
+credit_label.place(x=320,y=480)
+
+root.mainloop()
